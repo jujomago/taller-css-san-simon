74d57fd5dfb8184b269cf229a4512d43760c1c46
Josue <jujomago@gmail.com>
2021-10-25 19:30:07 -0400

    taller css, primer commit con flexBox

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h2>TAller de CSS</h2>
+      <h4>Container 1</h4>
+      <div className="flexContainer container1">
+        <div className="flexItem item1">Item 1</div>
+        <div className="flexItem item2">Item 2</div>
+        <div className="flexItem item3">Item 3</div>
+        <div className="flexItem item4">Item 4</div>
+        <div className="flexItem item5">Item 5</div>
+        <div className="flexItem item6">Item 6</div>
+      </div>
+      <h4>Container 2</h4>
+      <div className="flexContainer container2">
+        <div className="flexItem item1">Item 1</div>
+        <div className="flexItem item2">Item 2</div>
+        <div className="flexItem item3">Item 3</div>
+        <div className="flexItem item4">Item 4</div>
+        <div className="flexItem item5">Item 5</div>
+        <div className="flexItem item6">Item 6</div>
+      </div>
+      <h4>Container 3</h4>
+      <div className="flexContainer container3">
+        <div className="flexItem item1">Item 1</div>
+        <div className="flexItem item2">Item 2</div>
+        <div className="flexItem item3">Item 3</div>
+        <div className="flexItem item4">Item 4</div>
+        <div className="flexItem item5">Item 5</div>
+        <div className="flexItem item6">Item 6</div>
+      </div>
     </div>
   );
 }
